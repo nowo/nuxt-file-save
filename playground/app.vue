169df5acabab3869b0aafb0d9fa4bd73dbbb4ce0
cooj <z8888788@163.com>
2024-08-12 09:49:45 +0800

    chore: update deps, add loading to app.vue

--- playground/app.vue
+++ playground/app.vue
@@ -12,12 +12,13 @@
             <img src="/public/face_1.png" alt="">
             <!-- <img src="/face_1.png" alt=""> -->
         </div>
+        <div v-if="loading" class="loading">uploading...</div>
     </div>
 </template>
 
 <script lang="ts" setup>
 const src = ref()
-
+const loading = ref(false)
 const file = ref<File>()
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const handleFileChange = (e: any) => {
@@ -34,14 +35,29 @@ const submit = async () => {
 
     formData.append('files', file.value)
     formData.append('data', 'sds1042357')
-
+    loading.value = true
     const res = await $fetch<{ code: number, data: string, msg: string }>('/api/upload', {
         method: 'POST',
         body: formData,
     })
+    loading.value = false
     console.log(res)
-
     if (res.code !== 200) return alert(res.msg)
     src.value = res.data
 }
 </script>
+
+<style setup>
+.loading {
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, .3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+}
+</style>
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,10 @@
 export default defineNuxtConfig({
-    modules: ['../src/module'],
-    fileSave: {
-        mount: 'playground/public',
-    },
-    devtools: { enabled: true },
+  modules: ['../src/module'],
+
+  fileSave: {
+      mount: 'playground/public',
+  },
+
+  devtools: { enabled: true },
+  compatibilityDate: '2024-08-12',
 })
